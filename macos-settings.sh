b99ba5feefd25b5cbf75b9a148ac696e574bc33e
Jan Driesen <jndriesen.develop@gmail.com>
2018-06-27 22:58:34 +0200

    outsourced app store settings

--- macos-settings.sh
+++ macos-settings.sh
@@ -31,39 +31,6 @@ defaults write com.apple.ActivityMonitor SortDirection - int 0
 # enable secure keyboard entry in terminal.app
 defaults write com.apple.terminal SecureKeyboardEntry -bool true
 
-
-
-#############################################
-# Mac App Store								#
-#############################################
-
-# enable the webkit developer tools in the mac app store
-defaults write com.apple.appstore WebKitDeveloperExtras -bool true
-
-# enable debug menu in the mac app store
-defaults write com.apple.appstore ShowDebugMenu -bool true
-
-# enable the automatic update check
-defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
-
-# check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-# donwload newly available updates in background
-defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
-
-# install system data files & security updates
-defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
-
-# automatically download apps purchased on other macs
-defaults write com.apple.SoftwareUpdate ConfigDataInstall -int 1
-
-# turn on auto update
-defaults write com.apple.commerce AutoUpdate -bool true
-
-# allow the app store to reboot machine on macOS updates
-defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
-
 #########################################
 # Photos 								#
 #########################################
