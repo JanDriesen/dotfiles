1dc3e350ffb1ef69803177716c9a7b56e8e92381
Jan Driesen <jndriesen.develop@gmail.com>
2020-01-06 15:52:47 +0100

    added some cli functions, currently outcommented because they need to become refactored

---
+++ .funcs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# function to show processes with the given name
+#func psgrep() {
+#    ps up $(pgrep -f $@) 2>&-;
+#}
+
+# function to find the simulator folder for the given device argument
+#func findDeviceDir() {
+#	grep -r -e $0 --include="device.plist" ~/Library/Developer/CoreSimulator/Devices
+#}
+
+
+# function to change into the the specified simulator folder
+#func cdDeviceDir() {
+#	// todo
+#}
